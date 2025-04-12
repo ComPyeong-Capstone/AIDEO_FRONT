2c8e94a41a63f09ac9f50a78246160e943a37082
xinun <147150326+xinun@users.noreply.github.com>
2025-04-13 00:13:05 +0900

    안드 splash 화면

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React ,{ useEffect } from 'react';
+
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigator/AppNavigator';
 import {UserProvider} from './src/context/UserContext';
+import SplashScreen from "react-native-splash-screen";
 
 const App = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000); //스플래시 활성화 시간
+  });
   return (
     <UserProvider>
       <SafeAreaProvider>
