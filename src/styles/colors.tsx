d5156a2b7fddfab68b5310bd921bc31ea4bb19e7
xinun <147150326+xinun@users.noreply.github.com>
2025-05-06 17:38:30 +0900

    ㅅㅐㄱ ㅅㅜㅈㅓㅇ

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,11 +1,28 @@
 export const COLORS = {
-  primary: '#51BCB4',
+ primary: '#51BCB4',
   primary2: '#66D2CE',
-  secondary: '#273647',
-  background: 'FFFFF',
+  maincolor: '#51BCB4',
+  background: '#FFFFFF',
   textPrimary: 'black',
-  textSecondary: '#FFFFFF',
+  textSecondary: '#111111',
+  secondary: '#273647',
   buttonBackground: '#51BCB4',
   buttonText: '#1F2C3D',
   imagebox: '#0D1B2A',
+  black: '#000000',
+  thumbnail: '#4F637D',
+  videocontainer: '#273647',
+};
+
+export const DarkColors = {
+  primary: '#51BCB4',
+  primary2: '#66D2CE',
+  secondary: '#273647',
+  background: '#000100',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#AAAAAA',
+  buttonBackground: '#51BCB4',
+  buttonText: '#1F2C3D',
+  imagebox: '#0D1B2A',
+  black: '#000000',
 };
